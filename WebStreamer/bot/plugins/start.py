ed2c1a231dfa1432da9751e71deb057eb35b5111
JackSDavis <89790781+JackSDavis@users.noreply.github.com>
2021-12-23 06:04:16 -0800

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -7,11 +7,15 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 @StreamBot.on_message(filters.command(['start', 'help']))
 async def start(_, m: Message):
-    await m.reply(f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link.',
+    await m.reply(f'•Hello {m.from_user.mention(style="md")},
+ I can upload your files to direct download link send your file to upload!
+
+• سلام {m.from_user.mention(style="md")}
+من میتوانم فایل های شما را به لینک دانلود مستقیم تبدیل کنم فایل خود را ارسال کنید!',
                   reply_markup=InlineKeyboardMarkup(
                       [[
                             InlineKeyboardButton(
                                   f'{emoji.STAR} Source {emoji.STAR}',
-                                  url='https://github.com/EverythingSuckz/TG-FileStreamBot')
+                                  url='https://telegram.me/m_mahdihajizadeh')
                         ]]
                   ))
