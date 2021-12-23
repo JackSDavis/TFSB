67299943eca894a8737884a6d03dc57b8e008134
JackSDavis <89790781+JackSDavis@users.noreply.github.com>
2021-12-23 06:22:15 -0800

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -28,7 +28,7 @@ async def media_receive_handler(_, m: Message):
     log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
     stream_link = Var.URL + str(log_msg.message_id) + '/' +quote_plus(file_name) if file_name else ''
     await m.reply_text(
-    text="Download Link: {}".format(stream_link),
+    text="Your file uploaded!".format(stream_link),
         quote=True,
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Download', url=stream_link)]])
     )
